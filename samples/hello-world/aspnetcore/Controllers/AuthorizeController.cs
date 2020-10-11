9fc76b529576c064446a7e7e22bf710e52c75ca8
Nikola Malovic <nikola.malovic@outreach.io>
2020-10-11 22:31:55 +0200

    wip

---
+++ samples/hello-world/aspnetcore/Controllers/AuthorizeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Outreach.CXT.Demo.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorizeController : ControllerBase
+    {
+        private ILogger<AuthorizeController> logger;
+        private IHttpClientFactory httpClientFactory;
+
+        public AuthorizeController(ILogger<AuthorizeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            this.logger = logger;
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Login([FromQuery]string code)
+        {
+            
+
+            var userId = this.Request.Cookies[Constants.AUTH_USER_COOKIE_NAME];
+
+            using (var client = this.httpClientFactory.CreateClient())
+            {
+                
+            }
+
+
+            return Ok();
+        }
+    }
+}
