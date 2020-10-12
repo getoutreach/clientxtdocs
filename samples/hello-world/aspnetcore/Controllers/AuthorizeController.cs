8f1ec4282e2889400dd61eb1a2c5e32a341182a8
Nikola Malovic <nikola.malovic@outreach.io>
2020-10-12 10:04:04 +0200

    Authorize controller update

--- samples/hello-world/aspnetcore/Controllers/AuthorizeController.cs
+++ samples/hello-world/aspnetcore/Controllers/AuthorizeController.cs
@@ -1,6 +1,15 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Outreach.CXT.Demo.Server.Controllers
 {
@@ -9,28 +18,50 @@ namespace Outreach.CXT.Demo.Server.Controllers
     public class AuthorizeController : ControllerBase
     {
         private ILogger<AuthorizeController> logger;
-        private IHttpClientFactory httpClientFactory;
+        private readonly IConfiguration configuration;
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly IMemoryCache memoryCache;
 
-        public AuthorizeController(ILogger<AuthorizeController> logger, IHttpClientFactory httpClientFactory)
+        public AuthorizeController(ILogger<AuthorizeController> logger, IConfiguration configuration, IHttpClientFactory httpClientFactory, IMemoryCache memoryCache)
         {
             this.logger = logger;
+            this.configuration = configuration;
             this.httpClientFactory = httpClientFactory;
+            this.memoryCache = memoryCache;
         }
 
         [HttpGet]
-        public IActionResult Login([FromQuery]string code)
+        public async Task<ActionResult> Login([FromQuery]string code)
         {
-            
-
             var userId = this.Request.Cookies[Constants.AUTH_USER_COOKIE_NAME];
 
             using (var client = this.httpClientFactory.CreateClient())
             {
-                
+
+                var payload = new Dictionary<string, string>()
+                {
+                    { "client_id", this.configuration.GetValue<string>("APPLICATION_ID") },
+                    { "client_secret", this.configuration.GetValue<string>("APPLICATION_SECRET") },
+                    { "redirect_uri", this.configuration.GetValue<string>("REDIRECT_URI") },
+                    { "code", code },
+                    { "grant_type", "authorization_code"}
+                };
+
+                var request = await client.PostAsync("https://api.outreach.io/oauth/token", new FormUrlEncodedContent(payload));
+                request.EnsureSuccessStatusCode();
+
+                var content = await request.Content.ReadAsStringAsync();
+                var tokenInfo = JsonConvert.DeserializeObject<TokenInfo>(content);
+
+
+                this.memoryCache.Set(Constants.GetTokenCacheKey(userId), JsonConvert.SerializeObject(tokenInfo), DateTimeOffset.MaxValue);
+
+                // now we redirect to connect page
+                return this.Ok();
+
             }
 
-
-            return Ok();
+           return Ok();
         }
     }
 }
