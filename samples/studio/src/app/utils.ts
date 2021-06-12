3de290bd4ce0c704428a54a5d2365d9ed56bafe2
Nikola Malovic <nikola.malovic@outreach.io>
2021-06-12 16:25:02 +0200

    Using manifest url validation

--- samples/studio/src/app/utils.ts
+++ samples/studio/src/app/utils.ts
@@ -1,16 +1,3 @@
-import { Manifest } from '@outreach/client-addon-sdk';
-
-export const isUrl = (url: string): boolean => {
-    if (!url) {
-        return false;
-    }
-
-    // eslint-disable-next-line
-    return /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/.test(
-        url
-    );
-};
-
 export const downloadFile = (filename: string, content: string) => {
     const element = document.createElement('a');
     element.setAttribute(
@@ -25,18 +12,4 @@ export const downloadFile = (filename: string, content: string) => {
     element.click();
 
     document.body.removeChild(element);
-};
-
-export const generalInfoValid = (manifest: Manifest): boolean => {
-    return (
-        !!manifest &&
-        !!manifest.version &&
-        !!manifest.identifier &&
-        !!manifest.title.en &&
-        !!manifest.description.en &&
-        !!manifest.author.company &&
-        isUrl(manifest.author.privacyUrl) &&
-        isUrl(manifest.author.termsOfUseUrl) &&
-        isUrl(manifest.author.websiteUrl)
-    );
 };
