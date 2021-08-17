b13a817086f539997f3094e165abfd9acc474501
Nikola Malovic <nikola.malovic@outreach.io>
2021-08-17 13:58:04 +0200

    Minor changes

--- samples/beacon/src/App.tsx
+++ samples/beacon/src/App.tsx
@@ -3,17 +3,24 @@ import "./App.css";
 import addonSdk from "@outreach/client-addon-sdk";
 
 function App() {
-  const [counter, setCounter] = useState<number>(0);
+  const [counter, setCounter] = useState<number>(1);
 
   addonSdk.init().then((ctx) => {
     console.debug("[Beacon]::App -  addonSdk.initialized", { ctx });
 
+    addonSdk.decorate(counter.toString(), "badge");
+
     setTimeout(() => {
       const beacon = counter + 1;
       setCounter(beacon);
-      console.log("[Beacon]::timeout - decorate #", beacon);
+      console.debug("[Beacon]::timeout - decorate #", beacon);
       addonSdk.decorate(beacon.toString(), "badge");
     }, 10 * 1000);
+
+    setTimeout(() => {
+      console.debug("[Beacon]::timeout - notify #" + counter);
+      addonSdk.notify("Notification #" + counter, "info");
+    }, 5 * 1000);
   });
 
   return <div className="App">Beacon beat # {counter}</div>;
