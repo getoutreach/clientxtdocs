f4a85155732653422b08e979d1433f3024f6ac9a
Nikola Malovic <nikola.malovic@outreach.io>
2021-09-08 14:07:11 +0200

    Removing email restrection for demo puproses

--- samples/beacon/src/App.tsx
+++ samples/beacon/src/App.tsx
@@ -5,8 +5,10 @@ import addonSdk from "@outreach/client-addon-sdk";
 function App() {
   const [counter, setCounter] = useState<number>(1);
 
+  console.debug("[CXT][Beacon]::App -  intializing....");
+
   addonSdk.init().then((ctx) => {
-    console.debug("[Beacon]::App -  addonSdk.initialized", { ctx });
+    console.debug("[CXT][Beacon]::App -  addonSdk.initialized", { ctx });
 
     // https://github.com/getoutreach/clientxtsdk/blob/main/docs/manifest.md#context
     // If you have in addon manifest context=['usr.email'] (or any other user property you may need),
@@ -16,21 +18,23 @@ function App() {
     // In this sample, we are just performing a simple demo check if a user email of current Outreach user
     // is having some predefined value so only for that user addon will start working by
     // showing dummy toasts every 10 seconds and update badge decoration every 5 seconds
+    /*
     if (ctx.user?.email !== "nikola.malovic@outreach.io") {
       return;
     }
+    */
 
     addonSdk.decorate(counter.toString(), "badge");
 
     setTimeout(() => {
       const beacon = counter + 1;
       setCounter(beacon);
-      console.debug("[Beacon]::timeout - decorate #", beacon);
+      console.debug("[CXT][Beacon]::timeout - decorate #", beacon);
       addonSdk.decorate(beacon.toString(), "badge");
     }, 10 * 1000);
 
     setTimeout(() => {
-      console.debug("[Beacon]::timeout - notify #" + counter);
+      console.debug("[CXT][Beacon]::timeout - notify #" + counter);
       addonSdk.notify("Notification #" + counter, "info");
     }, 5 * 1000);
   });
